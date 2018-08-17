42506029c5a4396c9f6aa3d27e10b76fe72bceb5
Keith Richards <keith.richards@dish.com>
2018-08-17 09:23:59 -0600

    through time of possession

--- parsers.py
+++ parsers.py
@@ -21,11 +21,15 @@ class FloatParser(SimpleParser):
         setattr(self.team, self.name, float(data))
 
 class SplitParser(object):
-    def __init__(self, names, indexes, team, _type):
+    def __init__(self, names, team, _type, indexes):
         self.names = names
-        self.indexes = indexes
         self.team = team
         self._type = _type
+        self.indexes = indexes
+        if not self.indexes:
+            self.indexes = list(range(len(self.names)))
+        assert(isinstance(self.names, list))
+        assert(isinstance(self.indexes, list))
 
     def parse(self, data):
         d = data.split(' - ')
@@ -33,12 +37,12 @@ class SplitParser(object):
             setattr(self.team, name, self._type(d[i]))
 
 class SplitIntParser(SplitParser):
-    def __init__(self, names, indexes, team):
-        super().__init__(names, indexes, team, int)
+    def __init__(self, names, team, indexes=None):
+        super().__init__(names, team, int, indexes)
 
 class SplitFloatParser(SplitParser):
-    def __init__(self, names, indexes, team):
-        super().__init__(names, indexes, team, float)
+    def __init__(self, names, team, indexes=None):
+        super().__init__(names, team, float, indexes)
 
 
 class TeamHomePageParser(HTMLParser):
@@ -54,8 +58,73 @@ class TeamHomePageParser(HTMLParser):
             FloatParser('points_per_game', self.team),
             FloatParser('points_per_game_opp', self.team),
             None,
-            SplitIntParser(['games', 'points_scored'], [0, 1], self.team),
-            SplitIntParser(['points_scored_opp'], [1], self.team),
+            SplitIntParser(['games', 'points_scored'], self.team),
+            SplitIntParser(['points_scored_opp'], self.team, [1]),
+            None,
+            IntParser('first_downs', self.team),
+            IntParser('first_downs_opp', self.team),
+            None,
+            SplitIntParser(['rushing_first_downs', 'passing_first_downs',
+                            'penalty_first_downs'], self.team),
+            SplitIntParser(['rushing_first_downs_opp', 'passing_first_downs_opp',
+                            'penalty_first_downs_opp'], self.team),
+            None,
+            FloatParser('rushing_yards_per_attempt', self.team),
+            FloatParser('rushing_yards_per_attempt_opp', self.team),
+            None,
+            SplitIntParser(['rushing_attempts', 'rushing_yards', 'rushing_tds'], self.team),
+            SplitIntParser(['rushing_attempts_opp', 'rushing_yards_opp', 'rushing_tds_opp'], self.team),
+            None,
+            FloatParser('passing_rating', self.team),
+            FloatParser('passing_rating_opp', self.team),
+            None,
+            IntParser('passing_yards', self.team),
+            IntParser('passing_yards_opp', self.team),
+            None,
+            SplitIntParser(['passing_attempts', 'passing_completions',
+                            'passing_interceptions', 'passing_tds'], self.team),
+            SplitIntParser(['passing_attempts_opp', 'passing_completions_opp',
+                            'passing_interceptions_opp', 'passing_tds_opp'], self.team),
+            None,
+            FloatParser('total_offense_yards_per_play', self.team),
+            FloatParser('total_offense_yards_per_play_opp', self.team),
+            None,
+            SplitIntParser(['total_offense_plays', 'total_offense_yards'], self.team),
+            SplitIntParser(['total_offense_plays_opp', 'total_offense_yards_opp'], self.team),
+            None,
+            FloatParser('punt_return_yards_per_return', self.team),
+            FloatParser('punt_return_yards_per_return_opp', self.team),
+            None,
+            SplitIntParser(['punt_return_returns', 'punt_return_yards', 'punt_return_tds'], self.team),
+            SplitIntParser(['punt_return_returns_opp', 'punt_return_yards_opp', 'punt_return_tds_opp'],
+                           self.team),
+            None,
+            FloatParser('kickoff_return_yards_per_return', self.team),
+            FloatParser('kickoff_return_yards_per_return_opp', self.team),
+            None,
+            SplitIntParser(['kickoff_return_returns', 'kickoff_return_yards', 'kickoff_return_tds'],
+                           self.team),
+            SplitIntParser(['kickoff_return_returns_opp', 'kickoff_return_yards_opp', 'kickoff_return_tds_opp'],
+                           self.team),
+            None,
+            FloatParser('punting_yards_per_punt', self.team),
+            FloatParser('punting_yards_per_punt_opp', self.team),
+            None,
+            SplitIntParser(['punting_punts', 'punting_yards'], self.team),
+            SplitIntParser(['punting_punts_opp', 'punting_yards_opp'], self.team),
+            None,
+            SplitIntParser(['interception_returns', 'interception_yards', 'interception_tds'], self.team),
+            SplitIntParser(['interception_returns_opp', 'interception_yards_opp', 'interception_tds_opp'], self.team),
+            None,
+            SplitIntParser(['fumbles', 'fumbles_lost'], self.team),
+            SplitIntParser(['fumbles_opp', 'fumbles_lost_opp'], self.team),
+            None,
+            SplitIntParser(['penalties', 'penalties_yards'], self.team),
+            SplitIntParser(['penalties_opp', 'penalties_yards_opp'], self.team),
+            None,
+            StringParser('time_of_possession', self.team),
+            StringParser('time_of_possession_opp', self.team),
+
         ]
 
     def handle_starttag(self, tag, attrs):
--- tests/testTeamHomePageParser.py
+++ tests/testTeamHomePageParser.py
@@ -9,13 +9,35 @@ from parsers import TeamHomePageParser
 from parsers import SplitIntParser
 
 class TestSplitIntParser(unittest.TestCase):
-    def test_split_int(self):
+    def test_split_int_short(self):
         data = '0 - 12 - 273'
         o = MagicMock()
-        p = SplitIntParser(['k', 'e'], [1,2], o)
+        p = SplitIntParser(['k', 'e'], o, [1,2])
         p.parse(data)
         self.assertEqual(o.k, 12)
         self.assertEqual(o.e, 273)
+
+    def test_split_int_reverse(self):
+        data = '12 - 273'
+        o = MagicMock()
+        p = SplitIntParser(['k', 'e'], o, [1,0])
+        p.parse(data)
+        self.assertEqual(o.k, 273)
+        self.assertEqual(o.e, 12)
+
+    def test_split_int_middle(self):
+        data = '12 - 273 - 0'
+        o = MagicMock()
+        p = SplitIntParser(['k'], o, [1])
+        p.parse(data)
+        self.assertEqual(o.k, 273)
+
+    def test_split_int_only(self):
+        data = '12'
+        o = MagicMock()
+        p = SplitIntParser(['e'], o)
+        p.parse(data)
+        self.assertEqual(o.e, 12)
 
 class TestTeamHomePageParser(unittest.TestCase):
     @classmethod
@@ -34,78 +56,78 @@ class TestTeamHomePageParser(unittest.TestCase):
         self.assertEqual(self.team.games, 12)
     def test_points_scored(self):
         self.assertEqual(self.team.points_scored, 273)
-    # def test_first_downs(self):
-    #     self.assertEqual(self.team.first_downs, 238)
-    # def test_rushing_first_downs(self):
-    #     self.assertEqual(self.team.rushing_first_downs, 101)
-    # def test_passing_first_downs(self):
-    #     self.assertEqual(self.team.passing_first_downs, 120)
-    # def test_penalty_first_downs(self):
-    #     self.assertEqual(self.team.penalty_first_downs, 17)
-    # def test_rushing_yards_per_attempt(self):
-    #     self.assertEqual(self.team.rushing_yards_per_attempt, 4.80)
-    # def test_rushing_attempts(self):
-    #     self.assertEqual(self.team.rushing_attempts, 420)
-    # def test_rushing_yards(self):
-    #     self.assertEqual(self.team.rushing_yards, 2018)
-    # def test_rushing_tds(self):
-    #     self.assertEqual(self.team.rushing_tds, 15)
-    # def test_passing_rating(self):
-    #     self.assertEqual(self.team.passing_rating, 128.39)
-    # def test_passing_yards(self):
-    #     self.assertEqual(self.team.passing_yards, 2814)
-    # def test_passing_attempts(self):
-    #     self.assertEqual(self.team.passing_attempts, 423)
-    # def test_passing_completions(self):
-    #     self.assertEqual(self.team.passing_completions, 260)
-    # def test_passing_interceptions(self):
-    #     self.assertEqual(self.team.passing_interceptions, 8)
-    # def test_passing_tds(self):
-    #     self.assertEqual(self.team.passing_tds, 19)
-    # def test_total_offense_yards_per_play(self):
-    #     self.assertEqual(self.team.total_offense_yards_per_play, 5.73)
-    # def test_total_offense_plays(self):
-    #     self.assertEqual(self.team.total_offense_plays, 843)
-    # def test_total_offense_yards(self):
-    #     self.assertEqual(self.team.total_offense_yards, 4832)
-    # def test_punt_return_yards_per_return(self):
-    #     self.assertEqual(self.team.punt_return_yards_per_return, 5.00)
-    # def test_punt_return_yards(self):
-    #     self.assertEqual(self.team.punt_return_yards, 30)
-    # def test_punt_return_returns(self):
-    #     self.assertEqual(self.team.punt_return_returns, 6)
-    # def test_punt_return_tds(self):
-    #     self.assertEqual(self.team.punt_return_tds, 0)
-    # def test_kickoff_return_yards_per_return(self):
-    #     self.assertEqual(self.team.kickoff_return_yards_per_return, 19.80)
-    # def test_kickoff_return_yards(self):
-    #     self.assertEqual(self.team.kickoff_return_yards, 812)
-    # def test_kickoff_return_returns(self):
-    #     self.assertEqual(self.team.kickoff_return_returns, 41)
-    # def test_kickoff_return_tds(self):
-    #     self.assertEqual(self.team.kickoff_return_tds, 0)
-    # def test_punting_yards_per_punt(self):
-    #     self.assertEqual(self.team.punting_yards_per_punt, 42.07)
-    # def test_punting_punts(self):
-    #     self.assertEqual(self.team.punting_punts, 41)
-    # def test_punting_yards(self):
-    #     self.assertEqual(self.team.punting_yards, 812)
-    # def test_interception_returns(self):
-    #     self.assertEqual(self.team.interception_returns, 9)
-    # def test_interception_yards(self):
-    #     self.assertEqual(self.team.interception_yards, 130)
-    # def test_interception_tds(self):
-    #     self.assertEqual(self.team.interception_tds, 1)
-    # def test_fumbles(self):
-    #     self.assertEqual(self.team.fumbles, 16)
-    # def test_fumbles_lost(self):
-    #     self.assertEqual(self.team.fumbles_lost, 6)
-    # def test_penalties(self):
-    #     self.assertEqual(self.team.penalties, 93)
-    # def test_penalties_yards(self):
-    #     self.assertEqual(self.team.penalties_yards, 826)
-    # def test_time_of_possession(self):
-    #     self.assertEqual(self.team.time_of_possession, "31:28.17")
+    def test_first_downs(self):
+        self.assertEqual(self.team.first_downs, 238)
+    def test_rushing_first_downs(self):
+        self.assertEqual(self.team.rushing_first_downs, 101)
+    def test_passing_first_downs(self):
+        self.assertEqual(self.team.passing_first_downs, 120)
+    def test_penalty_first_downs(self):
+        self.assertEqual(self.team.penalty_first_downs, 17)
+    def test_rushing_yards_per_attempt(self):
+        self.assertEqual(self.team.rushing_yards_per_attempt, 4.80)
+    def test_rushing_attempts(self):
+        self.assertEqual(self.team.rushing_attempts, 420)
+    def test_rushing_yards(self):
+        self.assertEqual(self.team.rushing_yards, 2018)
+    def test_rushing_tds(self):
+        self.assertEqual(self.team.rushing_tds, 15)
+    def test_passing_rating(self):
+        self.assertEqual(self.team.passing_rating, 128.39)
+    def test_passing_yards(self):
+        self.assertEqual(self.team.passing_yards, 2814)
+    def test_passing_attempts(self):
+        self.assertEqual(self.team.passing_attempts, 423)
+    def test_passing_completions(self):
+        self.assertEqual(self.team.passing_completions, 260)
+    def test_passing_interceptions(self):
+        self.assertEqual(self.team.passing_interceptions, 8)
+    def test_passing_tds(self):
+        self.assertEqual(self.team.passing_tds, 19)
+    def test_total_offense_yards_per_play(self):
+        self.assertEqual(self.team.total_offense_yards_per_play, 5.73)
+    def test_total_offense_plays(self):
+        self.assertEqual(self.team.total_offense_plays, 843)
+    def test_total_offense_yards(self):
+        self.assertEqual(self.team.total_offense_yards, 4832)
+    def test_punt_return_yards_per_return(self):
+        self.assertEqual(self.team.punt_return_yards_per_return, 5.00)
+    def test_punt_return_yards(self):
+        self.assertEqual(self.team.punt_return_yards, 30)
+    def test_punt_return_returns(self):
+        self.assertEqual(self.team.punt_return_returns, 6)
+    def test_punt_return_tds(self):
+        self.assertEqual(self.team.punt_return_tds, 0)
+    def test_kickoff_return_yards_per_return(self):
+        self.assertEqual(self.team.kickoff_return_yards_per_return, 19.80)
+    def test_kickoff_return_yards(self):
+        self.assertEqual(self.team.kickoff_return_yards, 812)
+    def test_kickoff_return_returns(self):
+        self.assertEqual(self.team.kickoff_return_returns, 41)
+    def test_kickoff_return_tds(self):
+        self.assertEqual(self.team.kickoff_return_tds, 0)
+    def test_punting_yards_per_punt(self):
+        self.assertEqual(self.team.punting_yards_per_punt, 42.07)
+    def test_punting_punts(self):
+        self.assertEqual(self.team.punting_punts, 58)
+    def test_punting_yards(self):
+        self.assertEqual(self.team.punting_yards, 2440)
+    def test_interception_returns(self):
+        self.assertEqual(self.team.interception_returns, 9)
+    def test_interception_yards(self):
+        self.assertEqual(self.team.interception_yards, 130)
+    def test_interception_tds(self):
+        self.assertEqual(self.team.interception_tds, 1)
+    def test_fumbles(self):
+        self.assertEqual(self.team.fumbles, 16)
+    def test_fumbles_lost(self):
+        self.assertEqual(self.team.fumbles_lost, 6)
+    def test_penalties(self):
+        self.assertEqual(self.team.penalties, 93)
+    def test_penalties_yards(self):
+        self.assertEqual(self.team.penalties_yards, 826)
+    def test_time_of_possession(self):
+        self.assertEqual(self.team.time_of_possession, "31:28.17")
     # def test_third_down_conversion_attempts(self):
     #     self.assertEqual(self.team.third_down_conversion_attempts, 167)
     # def test_third_down_conversions(self):
@@ -131,82 +153,82 @@ class TestTeamHomePageParser(unittest.TestCase):
     # def test_two_point_conversions_made(self):
     #     self.assertEqual(self.team.two_point_conversions_made, 1)
 
-    # def test_points_per_game_opp(self):
-    #     self.assertEqual(self.team.points_per_game_opp, 33.9)
-    # def test_points_scored_opp(self):
-    #     self.assertEqual(self.team.points_scored_opp, 407)
-    # def test_first_downs_opp(self):
-    #     self.assertEqual(self.team.first_downs_opp, 274)
-    # def test_rushing_first_downs_opp(self):
-    #     self.assertEqual(self.team.rushing_first_downs_opp, 124)
-    # def test_passing_first_downs_opp(self):
-    #     self.assertEqual(self.team.passing_first_downs_opp, 129)
-    # def test_penalty_first_downs_opp(self):
-    #     self.assertEqual(self.team.penalty_first_downs_opp, 21)
-    # def test_rushing_yards_per_attempt_opp(self):
-    #     self.assertEqual(self.team.rushing_yards_per_attempt_opp, 5.25)
-    # def test_rushing_attempts_opp(self):
-    #     self.assertEqual(self.team.rushing_attempts_opp, 480)
-    # def test_rushing_yards_opp(self):
-    #     self.assertEqual(self.team.rushing_yards_opp, 2522)
-    # def test_rushing_tds_opp(self):
-    #     self.assertEqual(self.team.rushing_tds_opp, 23)
-    # def test_passing_rating_opp(self):
-    #     self.assertEqual(self.team.passing_rating_opp, 162.05)
-    # def test_passing_yards_opp(self):
-    #     self.assertEqual(self.team.passing_yards_opp, 2983)
-    # def test_passing_attempts_opp(self):
-    #     self.assertEqual(self.team.passing_attempts_opp, 334)
-    # def test_passing_completions_opp(self):
-    #     self.assertEqual(self.team.passing_completions_opp, 213)
-    # def test_passing_interceptions_opp(self):
-    #     self.assertEqual(self.team.passing_interceptions_opp, 9)
-    # def test_passing_tds_opp(self):
-    #     self.assertEqual(self.team.passing_tds_opp, 29)
-    # def test_total_offense_yards_per_play_opp(self):
-    #     self.assertEqual(self.team.total_offense_yards_per_play_opp, 6.76)
-    # def test_total_offense_plays_opp(self):
-    #     self.assertEqual(self.team.total_offense_plays_opp, 814)
-    # def test_total_offense_yards_opp(self):
-    #     self.assertEqual(self.team.total_offense_yards_opp, 5505)
-    # def test_punt_return_yards_per_return_opp(self):
-    #     self.assertEqual(self.team.punt_return_yards_per_return_opp, 7.44)
-    # def test_punt_return_yards_opp(self):
-    #     self.assertEqual(self.team.punt_return_yards_opp, 201)
-    # def test_punt_return_returns_opp(self):
-    #     self.assertEqual(self.team.punt_return_returns_opp, 27)
-    # def test_punt_return_tds_opp(self):
-    #     self.assertEqual(self.team.punt_return_tds_opp, 0)
-    # def test_kickoff_return_yards_per_return_opp(self):
-    #     self.assertEqual(self.team.kickoff_return_yards_per_return_opp, 20.36)
-    # def test_kickoff_return_yards_opp(self):
-    #     self.assertEqual(self.team.kickoff_return_yards_opp, 957)
-    # def test_kickoff_return_returns_opp(self):
-    #     self.assertEqual(self.team.kickoff_return_returns_opp, 47)
-    # def test_kickoff_return_tds_opp(self):
-    #     self.assertEqual(self.team.kickoff_return_tds_opp, 2)
-    # def test_punting_yards_per_punt_opp(self):
-    #     self.assertEqual(self.team.punting_yards_per_punt_opp, 40.36)
-    # def test_punting_punts_opp(self):
-    #     self.assertEqual(self.team.punting_punts_opp, 44)
-    # def test_punting_yards_opp(self):
-    #     self.assertEqual(self.team.punting_yards_opp, 1776)
-    # def test_interception_returns_opp(self):
-    #     self.assertEqual(self.team.interception_returns_opp, 8)
-    # def test_interception_yards_opp(self):
-    #     self.assertEqual(self.team.interception_yards_opp, 79)
-    # def test_interception_tds_opp(self):
-    #     self.assertEqual(self.team.interception_tds_opp, 1)
-    # def test_fumbles_opp(self):
-    #     self.assertEqual(self.team.fumbles_opp, 18)
-    # def test_fumbles_lost_opp(self):
-    #     self.assertEqual(self.team.fumbles_lost_opp, 6)
-    # def test_penalties_opp(self):
-    #     self.assertEqual(self.team.penalties_opp, 68)
-    # def test_penalties_yards_opp(self):
-    #     self.assertEqual(self.team.penalties_yards_opp, 619)
-    # def test_time_of_possession_opp(self):
-    #     self.assertEqual(self.team.time_of_possession_opp, "28:31.83")
+    def test_points_per_game_opp(self):
+        self.assertEqual(self.team.points_per_game_opp, 33.9)
+    def test_points_scored_opp(self):
+        self.assertEqual(self.team.points_scored_opp, 407)
+    def test_first_downs_opp(self):
+        self.assertEqual(self.team.first_downs_opp, 274)
+    def test_rushing_first_downs_opp(self):
+        self.assertEqual(self.team.rushing_first_downs_opp, 124)
+    def test_passing_first_downs_opp(self):
+        self.assertEqual(self.team.passing_first_downs_opp, 129)
+    def test_penalty_first_downs_opp(self):
+        self.assertEqual(self.team.penalty_first_downs_opp, 21)
+    def test_rushing_yards_per_attempt_opp(self):
+        self.assertEqual(self.team.rushing_yards_per_attempt_opp, 5.25)
+    def test_rushing_attempts_opp(self):
+        self.assertEqual(self.team.rushing_attempts_opp, 480)
+    def test_rushing_yards_opp(self):
+        self.assertEqual(self.team.rushing_yards_opp, 2522)
+    def test_rushing_tds_opp(self):
+        self.assertEqual(self.team.rushing_tds_opp, 23)
+    def test_passing_rating_opp(self):
+        self.assertEqual(self.team.passing_rating_opp, 162.05)
+    def test_passing_yards_opp(self):
+        self.assertEqual(self.team.passing_yards_opp, 2983)
+    def test_passing_attempts_opp(self):
+        self.assertEqual(self.team.passing_attempts_opp, 334)
+    def test_passing_completions_opp(self):
+        self.assertEqual(self.team.passing_completions_opp, 213)
+    def test_passing_interceptions_opp(self):
+        self.assertEqual(self.team.passing_interceptions_opp, 9)
+    def test_passing_tds_opp(self):
+        self.assertEqual(self.team.passing_tds_opp, 29)
+    def test_total_offense_yards_per_play_opp(self):
+        self.assertEqual(self.team.total_offense_yards_per_play_opp, 6.76)
+    def test_total_offense_plays_opp(self):
+        self.assertEqual(self.team.total_offense_plays_opp, 814)
+    def test_total_offense_yards_opp(self):
+        self.assertEqual(self.team.total_offense_yards_opp, 5505)
+    def test_punt_return_yards_per_return_opp(self):
+        self.assertEqual(self.team.punt_return_yards_per_return_opp, 7.44)
+    def test_punt_return_yards_opp(self):
+        self.assertEqual(self.team.punt_return_yards_opp, 201)
+    def test_punt_return_returns_opp(self):
+        self.assertEqual(self.team.punt_return_returns_opp, 27)
+    def test_punt_return_tds_opp(self):
+        self.assertEqual(self.team.punt_return_tds_opp, 0)
+    def test_kickoff_return_yards_per_return_opp(self):
+        self.assertEqual(self.team.kickoff_return_yards_per_return_opp, 20.36)
+    def test_kickoff_return_yards_opp(self):
+        self.assertEqual(self.team.kickoff_return_yards_opp, 957)
+    def test_kickoff_return_returns_opp(self):
+        self.assertEqual(self.team.kickoff_return_returns_opp, 47)
+    def test_kickoff_return_tds_opp(self):
+        self.assertEqual(self.team.kickoff_return_tds_opp, 2)
+    def test_punting_yards_per_punt_opp(self):
+        self.assertEqual(self.team.punting_yards_per_punt_opp, 40.36)
+    def test_punting_punts_opp(self):
+        self.assertEqual(self.team.punting_punts_opp, 44)
+    def test_punting_yards_opp(self):
+        self.assertEqual(self.team.punting_yards_opp, 1776)
+    def test_interception_returns_opp(self):
+        self.assertEqual(self.team.interception_returns_opp, 8)
+    def test_interception_yards_opp(self):
+        self.assertEqual(self.team.interception_yards_opp, 79)
+    def test_interception_tds_opp(self):
+        self.assertEqual(self.team.interception_tds_opp, 1)
+    def test_fumbles_opp(self):
+        self.assertEqual(self.team.fumbles_opp, 18)
+    def test_fumbles_lost_opp(self):
+        self.assertEqual(self.team.fumbles_lost_opp, 6)
+    def test_penalties_opp(self):
+        self.assertEqual(self.team.penalties_opp, 68)
+    def test_penalties_yards_opp(self):
+        self.assertEqual(self.team.penalties_yards_opp, 619)
+    def test_time_of_possession_opp(self):
+        self.assertEqual(self.team.time_of_possession_opp, "28:31.83")
     # def test_third_down_conversion_attempts_opp(self):
     #     self.assertEqual(self.team.third_down_conversion_attempts_opp, 164)
     # def test_third_down_conversions_opp(self):
